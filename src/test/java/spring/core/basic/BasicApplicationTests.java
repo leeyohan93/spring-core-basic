1fd7c128fa143fea316fb2e50a95e7e4eb81e371
yohan <good_1411@naver.com>
2020-09-30 14:27:52 +0900

    feat: init spring gradle project

---
+++ src/test/java/spring/core/basic/BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.core.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
