7b8226d7be689d1b06dfdc811ca223ea9d4a1ebb
yohan <good_1411@naver.com>
2020-10-03 15:08:03 +0900

    feat: lombok 적용

--- src/main/java/spring/core/basic/member/MemberServiceImpl.java
+++ src/main/java/spring/core/basic/member/MemberServiceImpl.java
@@ -1,16 +1,18 @@
 package spring.core.basic.member;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService {
     private final MemberRepository memberRepository;
 
-    @Autowired //ac.getBean(MemberRepository.class)
-    public MemberServiceImpl(final MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
+//    @Autowired //ac.getBean(MemberRepository.class)
+//    public MemberServiceImpl(final MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
 
     public void join(Member member) {
         memberRepository.save(member);
--- src/main/java/spring/core/basic/order/OrderServiceImpl.java
+++ src/main/java/spring/core/basic/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package spring.core.basic.order;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import spring.core.basic.discount.DiscountPolicy;
@@ -7,15 +8,16 @@ import spring.core.basic.member.Member;
 import spring.core.basic.member.MemberRepository;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
-    public OrderServiceImpl(final MemberRepository memberRepository, final DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    @Autowired
+//    public OrderServiceImpl(final MemberRepository memberRepository, final DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(final Long memberId, final String itemName, final int itemPrice) {
