a47c428814b7ee673ee5494cafef93495b9defee
yohan <good_1411@naver.com>
2020-10-04 17:58:58 +0900

    feat: 빈 생명주기 콜백
     - @PostConstruct , @PreDestroy 를 사용하지 못 할 경우에는
       @Bean의 옵션 설정 기능으로 initMethod와 closemMethod를 설정한다.

---
+++ src/test/java/spring/core/basic/lifecycle/NetworkClient.java
@@ -0,0 +1,46 @@
+package spring.core.basic.lifecycle;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class NetworkClient {
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+        connect();
+        call("초기화 연결 메시");
+    }
+
+    public void setUrl(final String url) {
+        this.url = url;
+    }
+
+    // 서비스 시작 시 호출
+    public void connect() {
+        System.out.println("connect: " + url);
+    }
+
+    public void call(String msg) {
+        System.out.println("call: " + url + " message = " + msg);
+    }
+
+    // 서비스 종료 시 호출
+    public void disconnect() {
+        System.out.println("close: " + url);
+    }
+
+    @PostConstruct
+    public void init(){
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @PreDestroy
+    public void close(){
+        System.out.println("NetworkClient.close");
+        disconnect();
+    }
+}
