ea63df365533e58b91ebba980f69e284135c7722
yohan <good_1411@naver.com>
2020-10-05 22:52:01 +0900

    feat: request bean 과 proxy 구현

---
+++ src/main/java/spring/core/basic/common/LogDemoService.java
@@ -0,0 +1,14 @@
+package spring.core.basic.common;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+    private final MyLogger myLogger;
+
+    public void logic(String id) {
+        myLogger.log("service id = " + id);
+    }
+}
